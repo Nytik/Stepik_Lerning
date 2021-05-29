b156652d325743c1bf080c871b6e47017645112a
Nytik <sanek-butko@mail.ru>
2021-05-29 18:03:06 +0300

    нихуя не понятно

--- stepik_auto/lesson3_6_step3.py
+++ stepik_auto/lesson3_6_step3.py
@@ -2,9 +2,11 @@ import pytest
 import math
 import time
 from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 
-answer = math.log(int(time.time()))
-code_link = ['236895', '236896', '236897', '236898', '236899', '236903', '236904', '236905']
 
 @pytest.fixture(scope="function")
 def browser():
@@ -14,12 +16,24 @@ def browser():
     print("\nquit browser..")
     browser.quit()
 
-@pytest.mark.parametrize('code', code_link)
-def test_guest_should_see_login_link(browser, code):
-    link = f"https://stepik.org/lesson/{code}/step/1"
-    browser.get(link)
-    # textarea = browser.find_element_by_css_selector("#ember85")
-    # textarea.send_keys(answer)
-    # button = browser.find_element_by_css_selector(".submit-submission")
-    # button.click()
+class TestLogin:
+    text = ''
+    cl = ['https://stepik.org/lesson/236895/step/1',
+          'https://stepik.org/lesson/236896/step/1',
+          'https://stepik.org/lesson/236897/step/1',
+          'https://stepik.org/lesson/236898/step/1',
+          'https://stepik.org/lesson/236899/step/1',
+          'https://stepik.org/lesson/236903/step/1',
+          'https://stepik.org/lesson/236904/step/1',
+          'https://stepik.org/lesson/236905/step/1']
+
+    @pytest.mark.parametrize('links', cl)
+    def test_guest_should_see_login_link(self, browser, links):
+        browser.get(links)
+        time.sleep(10)
+        textarea = browser.find_element_by_css_selector('//*[@placeholder="Напишите ваш ответ здесь..."]')
+        textarea.send_key(str(math.log(int(time.time()))))
+        #button = WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.XPATH, '.submit-submission')))
+        #button.click()
+
 
